ccb1524fe69a90d50826e26ca6a8b2e571549484
leonel47 <waboleonel@gmail.com>
2022-10-12 10:52:03 +0100

    Initial commit, ground zero

---
+++ src/main/java/com/wbt/learnsp/LearnspApplication.java
@@ -0,0 +1,13 @@
+package com.wbt.learnsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspApplication.class, args);
+	}
+
+}
